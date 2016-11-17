56651bde7d2db572f8e057326d3e200a966a6ec5
Stephen Emery and Tim Jung <stephen@>
2016-11-17 13:59:24 -0800

    Add front styling.

--- app/src/main/java/com/epicodus/alcshop/SplashScreenActivity.java
+++ app/src/main/java/com/epicodus/alcshop/SplashScreenActivity.java
@@ -1,10 +1,12 @@
 package com.epicodus.alcshop;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -12,11 +14,18 @@ import butterknife.ButterKnife;
 public class SplashScreenActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.welcomeButton) Button mWelcomeButton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView mAppNameTextView;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         ButterKnife.bind(this);
+
+        mAppNameTextView = (TextView) findViewById(R.id.storeName);
+        Typeface CasperFont = Typeface.createFromAsset(getAssets(), "fonts/BlackCasper.ttf");
+        mAppNameTextView.setTypeface(CasperFont);
 
         mWelcomeButton.setOnClickListener(this);
     }
