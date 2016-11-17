346f022447e81706001526294bb3cb4659b9b7c2
Stephen Emery and Tim Jung <stephen+tim@>
2016-11-17 09:24:03 -0800

    Add click listener to image button.

--- app/src/main/java/com/epicodus/alcshop/MainActivity.java
+++ app/src/main/java/com/epicodus/alcshop/MainActivity.java
@@ -1,13 +1,42 @@
 package com.epicodus.alcshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.beerButton) ImageButton mBeerButton;
+    @Bind(R.id.whiskeyButton) ImageButton mWhiskeyButton;
+    public static final String TAG = "log";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mBeerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Beer button clicked");
+            }
+        });
+
+        mWhiskeyButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Whiskey Button Clicked");
+            }
+        });
     }
+
+
 }
