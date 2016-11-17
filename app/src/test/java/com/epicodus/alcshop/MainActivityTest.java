fed211d18beaa2d5f5e3389ce520949703afd8d3
Stephen Emery and Tim Jung <stephen@>
2016-11-17 10:45:18 -0800

    Add robolectric tests.

--- app/src/test/java/com/epicodus/alcshop/MainActivityTest.java
+++ app/src/test/java/com/epicodus/alcshop/MainActivityTest.java
@@ -1,7 +1,51 @@
 package com.epicodus.alcshop;
 
-/**
- * Created by Guest on 11/17/16.
- */
+import android.content.Intent;
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
 public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView textView = (TextView) activity.findViewById(R.id.textView);
+        assertTrue("AlcShop".equals(textView.getText().toString()));
+    }
+
+    @Test
+    public void beerActivityStarted() {
+        activity.findViewById(R.id.beerButton).performClick();
+        Intent expectedIntent = new Intent(activity, BeerActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+
+    @Test
+    public void whiskeyActivityStarted() {
+        activity.findViewById(R.id.whiskeyButton).performClick();
+        Intent expectedIntent = new Intent(activity, WhiskeyActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
 }
